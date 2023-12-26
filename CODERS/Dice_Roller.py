0d067f889ec4b93f1db636f7b9f433136e5d75e6
Prakhar Vaish <73419181+VAISHPrakhar@users.noreply.github.com>
2023-12-27 01:24:04 +0530

    Update Dice_Roller.py

--- CODERS/Dice_Roller.py
+++ CODERS/Dice_Roller.py
@@ -1,8 +1,11 @@
 import random
-rolling=True 
-while rolling == True:
-    number=random.randrange(1,7)
-    print("You rolled a " + str(number) + "!!!")
-    again=input("do you want to continue? Say 'NO' to quit")
-    if again == "NO":
-        rolling= False
+
+def roll_dice_game():
+    user_choice = int(input("Guess the number on the dice (1-6): "))
+    dice_number = random.randint(1, 6)
+    if user_choice < 1 or user_choice > 6:
+        print( "Invalid input. Please choose a number between 1 and 6.")
+    if user_choice == dice_number:
+        print (f"Congratulations! The dice rolled {dice_number}. You guessed it right!")
+    else:
+        print (f"Sorry, the dice rolled {dice_number}. Better luck next time!")
